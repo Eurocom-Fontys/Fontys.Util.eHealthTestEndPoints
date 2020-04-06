d5f08ba07e9ae14ff554c1aa1c6bdb1ad8af81ab
Gertjan van Heertum <gertjan.vanheertum@eurocom-group.eu>
2020-04-06 09:25:28 +0200

    Small readme and some changes to the medicine dispenser code

--- MedicineDispenserService.cs
+++ MedicineDispenserService.cs
@@ -7,29 +7,31 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using EurocomFontysHealth.Library.Helpers;
 
 namespace EurocomFontysHealth
 {
     public static class MedicineDispenserService
     {
-        [FunctionName("MedicineDispenserService")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("MedicineDispenserGetAll")]
+        public static async Task<IActionResult> GetAll(
+             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "medicinedispenser/")] HttpRequest req,
+             ILogger log)
+        {
+            var res = new DataSource.MedicineDispenserDataSource().GetAll();
+            return new OkObjectResult(res);
+        }
+
+        [FunctionName("MedicineDispenserGetByID")]
+        public static async Task<IActionResult> GetAllGetByID(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "medicinedispenser/{id}/")] HttpRequest req,
+            string id,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var guid = GuidHelper.GetFromString(id);
+            if (guid == null) { return new BadRequestObjectResult("Invalid GUID"); }
+            var res = new DataSource.INRDeviceDataSource().GetByID(guid.Value);
+            return res != null ? (IActionResult)new OkObjectResult(res) : (IActionResult)new NotFoundObjectResult("No dispenser device found with ID");
         }
     }
 }
